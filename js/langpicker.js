83743277afd620781ebfa7f507488c6be743fe0d
Loïs Aubrée <lois.aubree@gmail.com>
2021-01-24 16:27:27 +0100

    Site updated: 2021-01-24 16:27:26

--- js/langpicker.js
+++ js/langpicker.js
@@ -1,3 +1,18 @@
-$(function(){
+$(function () {
     $('.selectpicker').selectpicker();
+    $('.selectpicker').on('change', function () {
+        var $selected = $('.selectpicker option:selected');
+        window.location.href = $selected[0].dataset.redirect;
+    });
+
+    initPageLanguage();
 });
+
+let initPageLanguage = function () {
+    if (document.documentElement.lang == 'en') {
+        $('.selectpicker').selectpicker('val', 'English');
+
+    } else if (document.documentElement.lang == 'fr') {
+        $('.selectpicker').selectpicker('val', 'Français');
+    }
+}
